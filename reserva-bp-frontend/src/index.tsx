98e6bfbced800071b13a73c851e41ec954be8135
Mateus Botelho <116606952+botelllhx@users.noreply.github.com>
2025-01-24 17:38:05 -0300

    Update index.tsx

--- reserva-bp-frontend/src/index.tsx
+++ reserva-bp-frontend/src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Use 'react-dom/client' para versões modernas
+import ReactDOM from 'react-dom/client'; 
 import './styles/index.css';
 import App from './App';
 
-const rootElement = document.getElementById('root'); // Certifique-se de que o ID corresponde ao do arquivo public/index.html
+const rootElement = document.getElementById('root');
 if (rootElement) {
-    const root = ReactDOM.createRoot(rootElement); // Use createRoot
+    const root = ReactDOM.createRoot(rootElement);
     root.render(
         <React.StrictMode>
             <App />
